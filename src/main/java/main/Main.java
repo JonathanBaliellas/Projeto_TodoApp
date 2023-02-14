74a51824ebe59bcabc4df947dc73b4b90f23d415
Jonathan Baliellas <j.baliellas@hotmail.com>
2023-02-14 14:09:08 -0300

    Update Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,11 @@
 package main;
 
+import view.LoginScreen;
+
 public class Main {
     public static void main(String[] args) {
-        
+        //Inicia a aplicação
+        LoginScreen run = new LoginScreen();
+        run.setVisible(true);
     }
 }
